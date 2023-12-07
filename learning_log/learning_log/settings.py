4c9bd0ab61b01d7ee7a5c6636dc9b4c51858a82d
Oluwadhara <olufemioluwadara@gmail.com>
2023-12-07 18:05:12 +0100

    Ready for deployment to heroku.

--- learning_log/learning_log/settings.py
+++ learning_log/learning_log/settings.py
@@ -150,7 +150,7 @@ if os.getcwd() == '/app':
     ALLOWED_HOSTS = ['*']
     # Static asset configuration
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = BASE_DIR / 'staticfiles'
+    STATIC_ROOT = BASE_DIR / 'static'
 
     STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'static'),
